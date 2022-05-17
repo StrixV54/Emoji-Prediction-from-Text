a97bee22cfbca56a0419ede64a62963ed044f804
Shrikant Vishwakarma <shrikantvk54@gmail.com>
2022-05-17 09:17:51 +0530

    Added new Code

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# Emoji-Prediction-from-Text
+# Text2Emoji :relaxed:
+![Emoji Representative](https://twistedsifter.files.wordpress.com/2015/06/the-best-text-emoticons-on-a-single-page.jpg)
+Text2Emoji is a text classification model trained on 200 sentences across 5 classes. Below fig. shows the list of emoji's on which the model is trained on. Text to the side represents a high-level emotion that the emoji depicts. :sunglasses:
+
+## Steps to Run
+1. `$> python3 main.py`
+2. Open emoji.html in the browser and start typing :speech_balloon:
+
+## Demo
+![Text2Emoji Demo](https://github.com/prakhar21/Text2Emoji/blob/master/demo.gif)
+-
+
+## Methodology
+![Flair Internal](https://github.com/prakhar21/Text2Emoji/blob/master/flair_internal1.png)
+
+
+## Model Architecture
+```
+Model: "TextClassifier(
+  (document_embeddings): DocumentRNNEmbeddings(
+    (embeddings): StackedEmbeddings(
+      (list_embedding_0): WordEmbeddings('glove')
+      (list_embedding_1): FlairEmbeddings(
+        (lm): LanguageModel(
+          (drop): Dropout(p=0.05, inplace=False)
+          (encoder): Embedding(300, 100)
+          (rnn): LSTM(100, 2048)
+          (decoder): Linear(in_features=2048, out_features=300, bias=True)
+        )
+      )
+      (list_embedding_2): FlairEmbeddings(
+        (lm): LanguageModel(
+          (drop): Dropout(p=0.05, inplace=False)
+          (encoder): Embedding(300, 100)
+          (rnn): LSTM(100, 2048)
+          (decoder): Linear(in_features=2048, out_features=300, bias=True)
+        )
+      )
+      (list_embedding_3): ELMoEmbeddings(model=elmo-medium)
+    )
+    (word_reprojection_map): Linear(in_features=5732, out_features=256, bias=True)
+    (rnn): LSTM(256, 512)
+    (dropout): Dropout(p=0.5, inplace=False)
+  )
+  (decoder): Linear(in_features=512, out_features=5, bias=True)
+  (loss_function): CrossEntropyLoss()
+)"
+```
+
+## Technologies Used
+1. Flask
+2. Flair
+3. HTML/Bootstrap
+4. Js
+
+All the code has been run on [Intel Dev Cloud](https://software.intel.com/en-us/devcloud)
---
+++ train.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+#from flair.data_fetcher import NLPTaskDataFetcher
+from flair.datasets import ClassificationCorpus
+from flair.embeddings import WordEmbeddings, FlairEmbeddings, DocumentRNNEmbeddings, ELMoEmbeddings
+from flair.models import TextClassifier
+from flair.trainers import ModelTrainer
+from pathlib import Path
+
+def segment_data(data_file):
+    try:
+        import pandas as pd
+    except ImportError:
+        raise
+    
+    data = pd.read_csv(data_file, encoding='latin-1').sample(frac=1).drop_duplicates()
+    data = data[['classes', 'title']].rename(columns={"classes":"label", "title":"text"})
+    data['label'] = '__label__' +data['label'].astype(str)
+    data['text'] = data['text'].apply(lambda k: k.lower().strip())
+    data.to_csv('./data/whole.csv', sep='\t', index = False, header = False)
+    data.iloc[0:int(len(data)*0.8)].to_csv('./data/train.csv', sep='\t', index = False, header = False)
+    data.iloc[int(len(data)*0.8):int(len(data)*0.9)].to_csv('./data/test.csv', sep='\t', index = False, header = False)
+    data.iloc[int(len(data)*0.9):].to_csv('./data/dev.csv', sep='\t', index = False, header = False)
+    return
+    
+def create_corpus():
+    if os.path.exists('./data/train.csv') and  os.path.exists('./data/test.csv') and  os.path.exists('./data/dev.csv'):
+        corpus = ClassificationCorpus(Path('./data'), test_file='test.csv', dev_file='dev.csv', train_file='train.csv') # NLPTaskDataFetcher.load_classification_corpus(Path('./data'), test_file='test.csv', dev_file='dev.csv', train_file='train.csv')
+        return corpus
+    else: return 0
+    
+def initialize_embeddings():
+    """
+    Summary:
+        Stacks the list of pre-trained embedding vectors to be used as word representation (in concat.)
+    Return:
+        list: Returns list of pretrained embeddings vectors
+    """
+    word_embeddings = [
+			WordEmbeddings('glove'),
+			FlairEmbeddings('news-forward'),
+			FlairEmbeddings('news-backward'),
+                        ELMoEmbeddings('medium')
+		]
+    return word_embeddings
+
+
+if __name__ == '__main__':
+    data_file = './data/emoji_train.csv'
+    segment_data(data_file)
+    corpus = create_corpus()
+
+    if not corpus:
+        raise
+
+    word_embeddings = initialize_embeddings()
+    document_embeddings = DocumentRNNEmbeddings(word_embeddings, hidden_size=512, reproject_words=True, reproject_words_dimension=256, rnn_type='LSTM', rnn_layers=1, bidirectional=False)
+    classifier = TextClassifier(document_embeddings, label_dictionary=corpus.make_label_dictionary('class'), multi_label=False, label_type='class')
+    trainer = ModelTrainer(classifier, corpus)
+    trainer.train('./model', max_epochs=20, patience=5, mini_batch_size=32, learning_rate=0.1)
+
